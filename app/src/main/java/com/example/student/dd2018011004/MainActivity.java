42684465959fee1ab8fb823090fc09ef3d71668d
kcli2412 <kcli2412@gmail.com>
2018-01-11 11:00:51 +0800

    add Menu

--- app/src/main/java/com/example/student/dd2018011004/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018011004/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.student.dd2018011004;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
@@ -38,63 +40,75 @@ public class MainActivity extends AppCompatActivity {
         lv = findViewById(R.id.listView);
     }
 
-    public void click1(View v)
-    {
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                String str_url = "https://www.mobile01.com/rss/news.xml";
-                URL url = null;
-                try {
-                    url = new URL(str_url);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("GET");
-                    conn.connect();
-                    InputStream inputStream = conn.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(inputStream);
-                    BufferedReader br = new BufferedReader(isr);
-                    StringBuilder sb = new StringBuilder();
-                    String str;
-                    while((str = br.readLine()) != null)
-                    {
-                        sb.append(str);
-                    }
-                    String str1 = sb.toString();
-                    Log.d("NET", str1);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-                    final MyHandler dataHandler = new MyHandler();
-                    SAXParserFactory spf = SAXParserFactory.newInstance();
-                    SAXParser sp = spf.newSAXParser();
-                    XMLReader xr = sp.getXMLReader();
-                    xr.setContentHandler(dataHandler);
-                    xr.parse(new InputSource(new StringReader(str1)));
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.menu_reload:
+                new Thread(){
+                    @Override
+                    public void run() {
+                        super.run();
+                        String str_url = "https://www.mobile01.com/rss/news.xml";
+                        URL url = null;
+                        try {
+                            url = new URL(str_url);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.connect();
+                            InputStream inputStream = conn.getInputStream();
+                            InputStreamReader isr = new InputStreamReader(inputStream);
+                            BufferedReader br = new BufferedReader(isr);
+                            StringBuilder sb = new StringBuilder();
+                            String str;
+                            while((str = br.readLine()) != null)
+                            {
+                                sb.append(str);
+                            }
+                            String str1 = sb.toString();
+                            Log.d("NET", str1);
 
-                    br.close();
-                    isr.close();
-                    inputStream.close();
+                            final MyHandler dataHandler = new MyHandler();
+                            SAXParserFactory spf = SAXParserFactory.newInstance();
+                            SAXParser sp = spf.newSAXParser();
+                            XMLReader xr = sp.getXMLReader();
+                            xr.setContentHandler(dataHandler);
+                            xr.parse(new InputSource(new StringReader(str1)));
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            adapter = new ArrayAdapter<String>(MainActivity.this,
-                                    android.R.layout.simple_list_item_1, dataHandler.titles);
-                            lv.setAdapter(adapter);
+                            br.close();
+                            isr.close();
+                            inputStream.close();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    adapter = new ArrayAdapter<String>(MainActivity.this,
+                                            android.R.layout.simple_list_item_1, dataHandler.titles);
+                                    lv.setAdapter(adapter);
+                                }
+                            });
+
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (SAXException e) {
+                            e.printStackTrace();
+                        } catch (ParserConfigurationException e) {
+                            e.printStackTrace();
                         }
-                    });
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (SAXException e) {
-                    e.printStackTrace();
-                } catch (ParserConfigurationException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+                    }
+                }.start();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
